9d6f7a4f98e7474f2308a562547dda59339abbe8
Yonatan Cohen <yonatan.cohen@outlook.com>
2024-12-18 00:18:20 +0200

    Yonatan added fies to projects finished Board class (tommorow will finish piece class and complete all unfinished methods)

---
+++ ChessProject/ChessProject/Board.cpp
@@ -0,0 +1,53 @@
+#include "Board.h"
+
+Board::Board()
+{
+	this->_pieces = new Piece[64];
+	this->_width = 8;
+	this->_length = 8;
+	this->_startingColor = 0; // white
+	this->_whiteKing = new King;
+	this->_blackKing = new King;
+	this->_turnNum = 0;
+}
+
+Board::Board(Piece* pieces, int width, int length, int startingColor, King* whiteKing, King* blackKing)
+{
+	this->_pieces = pieces;
+	this->_width = width;
+	this->_length = length;
+	this->_startingColor = startingColor;
+	this->_whiteKing = whiteKing;
+	this->_blackKing = blackKing;
+	this->_turnNum = 0;
+}
+
+Board::~Board()
+{
+	delete this->_blackKing;
+	delete this->_whiteKing;
+	delete this->_pieces;
+}
+
+Piece* Board::getPieces() const { return this->_pieces; }
+int Board::getWidth() const { return this->_width; }
+int Board::getLength() const { return this->_length; }
+int Board::getStartingColor() const { return this->_startingColor; }
+King* Board::getWhiteKing() const { return this->_whiteKing; }
+King* Board::getBlackKing() const { return this->_blackKing; }
+
+//bool Board::isEmpty(Cord c) const
+//{
+//	int index = c.getX() * c.getY();
+//	if (this->_pieces[index].getType == '#') return true;
+//	else return false;
+//}
+
+//void Board::update(std::string info)
+//{
+//	std::string cordStr = info.substr(info.length() - 2);
+//	Cord destCord = Cord::stringToCord(cordStr);
+//	int index = destCord.getX() * destCord.getY();
+//	if (this->_pieces[index].move(destCord) == true) this->_turnNum++;
+//	else error();
+//}
---
+++ ChessProject/ChessProject/Board.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Piece.h"
+#include "King.h"
+#include "Cord.h"
+
+class Piece;
+class King;
+class Cord;
+
+class Board
+{
+private:
+	Piece* _pieces;
+
+	int _width;
+	int _length;
+	int _startingColor;
+
+	King* _whiteKing;
+	King* _blackKing;
+
+public:
+	static int _turnNum;
+
+	Board();
+	Board(Piece* pieces, int width, int length, int startingColor, King* whiteKing, King* blackKing);
+	~Board();
+
+	Piece* getPieces() const;
+	int getWidth() const;
+	int getLength() const;
+	int getStartingColor() const;
+	King* getWhiteKing() const;
+	King* getBlackKing() const;
+
+	bool isEmpty(Cord c) const;
+	void update(std::string info);
+
+	//fix later
+	//std::string reciveFronendInfo(Pipe& inputPipe);
+	//void sendFrontendInfo(Pipe& inputPipe);
+};
