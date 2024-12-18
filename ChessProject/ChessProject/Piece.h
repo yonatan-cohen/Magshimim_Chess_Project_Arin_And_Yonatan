b6a5228b19be1e7831f2f9083ba42401ac31b6c0
Yonatan Cohen <yonatan.cohen@outlook.com>
2024-12-19 00:20:29 +0200

    Yonatan completed Piece.h

--- ChessProject/ChessProject/Piece.h
+++ ChessProject/ChessProject/Piece.h
@@ -1,6 +1,28 @@
 #pragma once
 
+#include "Cord.h"
+
 class Piece
 {
+private:
+	Cord* _position;
+	char _type;
+	bool isBlack;
+	Board* _board;
 
+public:
+	Piece();
+	Piece(Cord* pos, char type, bool isBlack, Board board);
+	Piece(const Piece& other);
+
+	~Piece();
+	Piece& operator=(const Piece& other);
+
+	Cord* getCord() const;
+	char getType() const;
+	bool getIsBlack() const;
+	Board* getBoard() const;
+
+	bool move(Cord dest);
+	virtual bool isValidMove(Cord dest) = 0;
 };
